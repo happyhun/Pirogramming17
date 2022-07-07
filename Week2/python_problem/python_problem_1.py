eed4869caea01c4a9a9b7746140c478642fe2ae0
Younghoon Hwang <younghun.ai@gmail.com>
2022-07-07 18:56:42 +0900

    1-3

--- Week2/python_problem/python_problem_1.py
+++ Week2/python_problem/python_problem_1.py
@@ -1,2 +1,13 @@
 num = 0
-count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+
+while True:
+    try:
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        count = int(count)
+        if count < 1 or count > 3:
+            raise Exception("1,2,3 중 하나를 입력하세요")
+        break
+    except ValueError:
+        print("정수를 입력하세요")
+    except Exception as e:
+        print(e)
