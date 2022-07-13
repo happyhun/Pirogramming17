ea905d2a503da4297a8e7a9a2cf928ce25701c4f
Younghoon Hwang <younghun.ai@gmail.com>
2022-07-13 13:16:19 +0900

    (myvenv 깃에서 제거)

--- Week3/djangogirls/myvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\hun48\onedrive\바탕 화면\develop\피로그래밍\hwangyounghoon\week3\djangogirls\myvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
